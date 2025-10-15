76cedbab1e19590c4b5c44168b83600619ad6898
gretaramadani <gretaramadani2008@gmail.com>
2025-10-15 20:19:57 +0200

    Database connection

---
+++ Module_7/dbconnect.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = 'local';
+$user = 'root';
+$password = '';
+
+try {
+    $conn = new PDO("mysql:host = $host; , $user, $password");
+    echo "Connected";
+}catch (Exception $e) {
+    echo "Not connected";
+}
+?>
